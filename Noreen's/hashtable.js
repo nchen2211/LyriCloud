d5ef48a4d04b684e1b29899de8323a17e00fe9c4
nchen2211 <nchrysilla@gmail.com>
2017-02-24 23:45:00 -0800

    hashtable done

--- Noreen's/hashtable.js
+++ Noreen's/hashtable.js
@@ -1,3 +1,7 @@
+/*
+ this function gets a string array, calculate each string frequency,
+ and sorted in descending order based on the highest frequency
+ */
 function search() {
     var data = document.getElementById('txt').value;
     var temp = data;
@@ -5,22 +9,39 @@ function search() {
     words = temp.split(" ");
     var uniqueWords = new Array();
     var count = new Array();
+    var wordMap = new Map();
+    var wordArray = new Array();
 
-
+    // calculate each word frequency
+    console.log(words.length);
     for (var i = 0; i < words.length; i++) {
-        //var count=0;
-        var f = 0;
+        var freq = 0;
         for (j = 0; j < uniqueWords.length; j++) {
             if (words[i] == uniqueWords[j]) {
                 count[j] = count[j] + 1;
-                //uniqueWords[j]=words[i];
-                f = 1;
+                freq = 1;
             }
         }
-        if (f == 0) {
+        if (freq == 0) {
             count[i] = 1;
             uniqueWords[i] = words[i];
         }
-        console.log("count of ->" + uniqueWords[i] + "<- - " + count[i]);
     }
+
+    for (var i=0; i<uniqueWords.length; i++) {
+        if (count[i] != "undefined") {
+            wordArray.push({word: uniqueWords[i], freq: count[i]});
+            wordMap.set(uniqueWords[i], count[i]);
+        }
+    }
+
+    // sorting
+    wordArray.sort(function(first,second){
+        return second.freq - first.freq;
+    });
+
+    for (var i=0; i<wordArray.length; i++) {
+        console.log(wordArray[i]);
+    }
+
 }
