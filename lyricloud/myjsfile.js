608f4f2252549b5efa878f49f8d230b75bee1341
Lernik Tahmasian <ltahmasi@usc.edu>
2017-02-25 17:19:06 -0800

    cloud shows

--- lyricloud/myjsfile.js
+++ lyricloud/myjsfile.js
@@ -1,19 +1,44 @@
 // JavaScript Document
 
 function Show()
-    { 
-		if(document.getElementById("search").text == "john"){
-			alert("hey john");
+{ 
+		if(document.getElementById("search").value == "john"){
+			
 			document.getElementById("artistNames").style.visibility="visible";
+			document.getElementById("wordcloudimg").style.visibility="hidden";
 		}
 		else{
+			document.getElementById("artistNames").style.visibility="hidden";
+			document.getElementById("wordcloudimg").style.visibility="visible";
 			document.getElementById("addbutton").style.visibility="visible";
 			document.getElementById("sharebutton").style.visibility="visible"; 
 			document.getElementById("gobutton").className = document.getElementById("gobutton").className + " move";
-			document.getElementById("wordcloud").src="../wordcloud.png";
+			document.getElementById("wordcloudimg").src="images/wordcloud.png";
 		}
-	}
-	 
+}
+
+function inputchange( i){
+	
+	var text = document.getElementsByTagName('a')[i].innerHTML;
+	document.getElementById("search").value=text;			   
+
+}
+/*
 	$("gobutton").click(function() {
     $(".gobutton").toggleClass("buttonLeft");
-});
+	});
+*/
+
+	
+	
+	function showCloud(){
+		document.getElementById("wordcloudimg").style.visibility="hidden";
+			
+		d3.wordcloud()
+			.size([500, 300])
+			.fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
+			.words(words)
+			.start();
+
+	}
+   
