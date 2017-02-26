0a40243e16322dec115c2a0e87a572a1460d4115
nchen2211 <nchrysilla@gmail.com>
2017-02-25 17:45:10 -0800

    hashtable

--- Noreen's/hashtable.js
+++ Noreen's/hashtable.js
@@ -1,3 +1,18 @@
+var pronouns = ["your","I","they","their","we","them",
+    "its","our","my","those","he","us","her","something",
+    "me","yourself","someone","everything","itself","everyone",
+    "themselves","anyone","him","whose","myself","everybody",
+    "ourselves","himself","somebody","yours","herself","whoever",
+    "you","that","it","this","what","which","these","his","she",
+    "lot","anything","whatever","nobody","none","mine","anybody",
+    "some","there","all","where","another","same","certain","nothing",
+    "self","nowhere","with","at","from","into","during","including",
+    "until","against","among","throughout","despite","towards","upon",
+    "concerning","of","to","in","for","on","by","about","like","through",
+    "over","before","between","after","since","without","under","within",
+    "along","following","across","behind","beyond","plus","except","but",
+    "up","out","around","down","off","above","near","the","a","one","some","few"]
+
 /*
  this function gets a string array, calculate each string frequency,
  and sorted in descending order based on the highest frequency
@@ -7,9 +22,10 @@ function search() {
     var temp = data;
     var words = new Array();
     words = temp.split(" ");
+    words = tolower(words);
+
     var uniqueWords = new Array();
     var count = new Array();
-    var wordMap = new Map();
     var wordArray = new Array();
 
     // calculate each word frequency
@@ -29,19 +45,43 @@ function search() {
     }
 
     for (var i=0; i<uniqueWords.length; i++) {
-        if (count[i] != "undefined") {
+        if (count[i] > 0) {
             wordArray.push({word: uniqueWords[i], freq: count[i]});
-            wordMap.set(uniqueWords[i], count[i]);
         }
     }
 
     // sorting
-    wordArray.sort(function(first,second){
+    wordArray.sort(function(first,second) {
         return second.freq - first.freq;
     });
 
+    console.log("BEFORE DELETION")
     for (var i=0; i<wordArray.length; i++) {
         console.log(wordArray[i]);
     }
 
+    // eliminating pronouns
+    for (var i=0; i<wordArray.length; i++) {
+        for (var j=0; j<pronouns.length; j++) {
+            if (wordArray[i].word == pronouns[j]) {
+                console.log("deleting: " + wordArray[i].word);
+                wordArray.splice(i,0);
+            }
+        }
+    }
+
+    console.log("AFTER DELETION")
+    for (var i=0; i<wordArray.length; i++) {
+        console.log(wordArray[i]);
+    }
+}
+
+// convert words found to lower case
+function tolower(strArray) {
+    var lowerTemp = new Array();
+    for (var i=0; i<strArray.length; i++) {
+        lowerTemp[i] = strArray[i].toLowerCase();
+    }
+
+    return lowerTemp;
 }
