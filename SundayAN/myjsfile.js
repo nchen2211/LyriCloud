cb2ef52f19d73fbdbba0ebb4d765ae29a22cb684
nchen2211 <nchrysilla@gmail.com>
2017-02-27 21:36:17 -0800

    UI fixed

--- SundayAN/myjsfile.js
+++ SundayAN/myjsfile.js
@@ -30,81 +30,48 @@ function inputchange( i){
 */
 
 	
-//this function clear the current cloud and add a new one	
-function showCloud(){
-	var words = [];
-	localStorage.setItem("selectedWord", "");
-	var artist = document.getElementById('search').value;
-	console.log(artist);
-	// var test = localStorage.getItem("selsectedWord");
-	// console.log(test);
-	var string_to_process = loadAllLyrics(true, artist); 
-}
-//this functino keep adding to the current cloud
-function addArtist(){
-	console.log("the add artist function /n");
-	var artist = document.getElementById('search').value;
-	console.log(artist);
-	var string_to_process = loadAllLyrics(false, artist); 
-}
+	
+	function showCloud(){
+		document.getElementById("wordcloudimg").style.visibility="hidden";
+		var artist = document.getElementById('search').value;
+		console.log(artist);
 
-function loadAllLyrics(clear, in_name){
+		var string_to_process = loadAllLyrics(artist); 
+		console.log(string_to_process);
+
+		
+		d3.wordcloud()
+			.size([500, 300])
+			.fill(d3.scale.ordinal().range(["#9926b2", "#604865", "#6e08b5", "#504f50"]))
+			.words(words)
+			.start();
+			
+			document.getElementById("addbutton").style.visibility="visible";
+			document.getElementById("sharebutton").style.visibility="visible"; 
+			document.getElementById("gobutton").className = document.getElementById("gobutton").className + " move";
+			//document.getElementById("wordcloudimg").src="images/wordcloud.png";
+
+	}
+
+function loadAllLyrics(in_name){
+	var str = in_name;
+	var processed = "";
 	var returnValue = "";
 	var xmlhttp = new XMLHttpRequest();
 	xmlhttp.onreadystatechange = function() {
 		if(this.readyState == 4 && this.status ==200){
-			// console.log(this.responseText);
+			console.log(this.responseText);
 			 // var returnValue= JSON.parse(this.responseText); 
-			
-			returnValue= (this.responseText);
-			// console.log("this should be the lyrics");
-			console.log(returnValue);
-			
-
-			search(returnValue);
+			var returnValue= (this.responseText);
+			 // console.log(returnValue);
+			 search(returnValue);
 			 // alert("what the fu");
 
 		}
 	};
-	// console.log("this is clear"+ clear);
-	if(clear){
-		xmlhttp.open("GET", "testphp.php?artist_name="+in_name+"&clear=true", true);
-	}else{
-		console.log("about to send to php!");
-		xmlhttp.open("GET", "testphp.php?artist_name="+in_name+"&clear=false", true);
-	}
-	
+	xmlhttp.open("GET", "testphp.php?q="+str+"&", true);
 	xmlhttp.send();
 
 	return returnValue;
 }
-
-function processCloud(in_array){
-	// var current_words = localStorage.getItem("selectedWord");
-	// console.log(in_array.length);
-	var words = [];
-
-	for(var i = 0; i < in_array.length; i++){
-		// sconsole.log(in_array[i]);
-		var each = {text: in_array[i].word, size: in_array[i].freq }
-		words.push(each);
-	}
-	// console.log("this is the word array");
-	// console.log(words);
-	// var new_words = current_words.concat(words);
-	localStorage.setItem("selectedWord", words);
-	// console.log("this is our word<br>");
-	// console.log(words);
-	document.getElementById('wordcloud').innerHTML = '';
-
-
-	d3.wordcloud()
-			.size([500, 300])
-			.fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
-			.words(words)
-			.start();
-
-	document.getElementById('addbutton').setAttribute("style",""); 
-
-}
-
+   
